dfe032583490104548b29e9785c44149ef30c3de
king <369558172@qq.com>
2018-05-09 17:21:03 +0800

    feat: add company restful api
    NO

--- src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
+++ src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
@@ -1,5 +1,63 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Company;
+import com.example.employee.restfulapi.entity.Employee;
+import com.example.employee.restfulapi.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping(value = "/companies")
 public class CompanyController {
+    @Autowired
+    private CompanyRepository companyRepository;
+
     //在此处完成Company API
+    @GetMapping
+    public List<Company> getCompanyList() {
+        List<Company> c = companyRepository.findAll();
+        return companyRepository.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Company getCompany(@PathVariable(value = "id") Long id) {
+        return companyRepository.findOne(id);
+    }
+
+    @GetMapping(value = "/{id}/employees")
+    public Set<Employee> getCompanyEmployees(@PathVariable(value = "id") Long id) {
+        return companyRepository.findOne(id).getEmployees();
+    }
+
+    @GetMapping(value = "/page/{pageNo}/pageSize/{pageSize}")
+    public Page<Company> getCompanyPage(@PathVariable(value = "pageNo") int pageNo, @PathVariable(value = "pageSize") int pageSize) {
+        Pageable pageable = new PageRequest(pageNo, pageSize);
+        return companyRepository.findAll(pageable);
+    }
+
+    @PostMapping
+    public Company addCompany(Company company) {
+        return companyRepository.save(company);
+    }
+
+    @PutMapping(value = "/{id}")
+    public Company updateCompany(@PathVariable(value = "id") Long id,
+                                 @RequestParam(value = "companyName") String companyName,
+                                 @RequestParam(value = "employeesNumber") Integer employeesNumber) {
+        Company company = companyRepository.getOne(id);
+        company.setCompanyName(companyName);
+        company.setEmployeesNumber(employeesNumber);
+        return companyRepository.save(company);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteCompany(@PathVariable(value = "id") Long id) {
+        companyRepository.delete(id);
+    }
 }
